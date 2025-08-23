cc5e8029be71d130e9e0a40f20f612aacf77d4e3
Daniel Costa Carvalho Martins <danielcostacarvalhomartins06@gmail.com>
2025-08-23 00:07:01 -0300

    tsalerts update

--- src/alerts.js
+++ src/alerts.js
@@ -1,6 +1,5 @@
-function mostrarAlerta(mensagem: string): void {
+function mostrarAlerta(mensagem) {
     alert(mensagem);
 }
-
 mostrarAlerta("Olá, bem-vindo ao portifolio virtual do desenvolvedor Daniel Costa.");
-mostrarAlerta("Espero que goste do meu trabalho")
+mostrarAlerta("Espero que goste do meu trabalho");
