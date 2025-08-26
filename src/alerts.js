cc30c7e165c505bbf209420abac9590281a20319
Daniel Costa Carvalho Martins <danielcostacarvalhomartins06@gmail.com>
2025-08-25 23:59:42 -0300

    ts update

--- src/alerts.js
+++ src/alerts.js
@@ -1,5 +1,7 @@
-function mostrarAlerta(mensagem) {
-    alert(mensagem);
+function mostrarAlerta(menssagem) {
+    window.alert(menssagem);
 }
-mostrarAlerta("Olá, bem-vindo ao portifolio virtual do desenvolvedor Daniel Costa.");
-mostrarAlerta("Espero que goste do meu trabalho");
+;
+mostrarAlerta("Olá, seja bem-vindo ao portifolio virtual do desenvolvedor Daniel Costa.");
+mostrarAlerta("Aproveite para conhecer um pouco do meu trabalho.");
+mostrarAlerta("Espero que goste.");
